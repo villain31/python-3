c8ee7e436b92b99b31f607fd357b3eb993b1a402
villain31 <35004258+villain31@users.noreply.github.com>
2019-05-21 11:37:39 +0530

    Update ques2.py

--- Edureka Tutorial/ques2.py
+++ Edureka Tutorial/ques2.py
@@ -5,11 +5,12 @@ def getMaxSumSubList(ticketsList):
         num2 = 0
         num1List = []
         num2List = []
-        tempList = []
         for ticket in tickets:
+            # Dynamic Programming
             temp = num1
             num1 = max(num1, num2 + ticket)
             num2 = temp
+
             tempList = num1List
             if(temp != num1):
                 num1List = num2List + [ticket]
@@ -17,19 +18,21 @@ def getMaxSumSubList(ticketsList):
         maxSumList.append(num1)
         num1List.reverse()
         print(''.join(str(i) for i in num1List))
-    
+
 
 def main():
+    # Number of Test Cases
     T = int(input())
     ticketsList = []
     for t in range(T):
+        # Number of Villains and Players TODO: 'N' Not Required
         N = int(input())
-        ticket = []
+        # List of Tickets
         ticket = (list(map(int, input().split())))
         ticketsList.append(ticket)
-
+    # Finding Max Sum Sub List (Non Continous)
     getMaxSumSubList(ticketsList)
 
-main()
 
-
+if __name__ == "__main__":
+    main()
